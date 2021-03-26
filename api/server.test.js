e569818f18a4503f2d4aa2aedcc0a96f47b9b9d0
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-26 15:35:01 -0600

    wip, working on tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,18 @@
+const server = require('./server')
+const request = require('supertest')
+const db = require('../data/dbConfig')
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
 })
